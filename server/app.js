56d076d717b2f03cb7e5461816c8d36a77e6a737
mehmet halis <mehmethaliscicek@gmail.com>
2021-02-06 13:44:05 +0300

    create client

--- server/app.js
+++ server/app.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 
 const app = express();
-
+app.use(cors());
 //db
-const db=require('./helper/db')();
+const db = require('./helper/db')();
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphiql:true
+    graphiql: true
 }));
 
 app.listen(process.env.PORT, () => {
